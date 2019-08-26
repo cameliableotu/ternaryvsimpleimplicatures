6c7c383e348d23e3a835194d585a1eb92474548c
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-08-26 13:05:46 +0200

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -78,6 +78,40 @@ newText ("<p> When he guesses the shadow correctly, you have to reward the baby 
         .wait()
 )
 ;	    
+PennController.Template(PennController.GetTable("table_stage1.csv"),
+PennController.defaultTable.filter("Item","dog") ,
+row => PennController( "dog" ,
+newText( "Description" , row.Description )
+	,
+	       newImage("hugeapple", "hugeapple.png")
+        .settings.size(200,200)
+        // .print()
+	       ,
+	      newImage("bigapple", "bigapple.png")
+        .settings.size(200,200)
+        // .print()
+    ,
+	          newImage("smallapple", "smallapple.png")
+        .settings.size(200,200)
+        // .print()
+    ,
+    newCanvas(700,200)
+        .settings.add( 0 , 0 , getImage("hugeapple") )
+        .settings.add( 250 , 0 , getImage("bigapple") )
+	  .settings.add(500, 0 , getImage("smallapple") )
+        .print()
+	       ,
+	    
+newSelector()
+    .settings.add( getImage("hugeapple") , getImage ("bigapple"), getImage("smallapple") )
+    .settings.keys(          "F"    ,          "J")
+    .settings.log()
+    .wait()
+)
+
+
+.log( "ID" , getVar("ID") )
+
 
 
 PennController.SendResults( "send" );
